619cf1872d17a0557f2bdd7722cd3ceccd335c95
Vladislav Gorceac <vvgorceac@gmail.com>
2024-04-20 09:32:27 +0300

    Small cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
         baseURL: process.env.baseURL ? process.env.baseURL : 'http://google.com/',
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
+        screenshot: 'on'
     },
 
     /* Configure projects for major browsers */
